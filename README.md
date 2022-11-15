72e6a14927846d9ef53e55eb1f1e59b426208961
chcp <chcp@cin.ufpe.br>
2022-11-15 09:29:48 -0300

    sh: change locust file to run 3 times with diff n_users
    - add a wip README.md file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bench-FastAPI
+
+> docker run -d -p 1521:1521 -e ORACLE_PASSWORD=<PASS> \
+  -v oracle-volume:/opt/oracle/oradata gvenzl/oracle-xe
+
+> sh scripts/01_start_prod.sh
+
+> sh scripts/02_locust.sh
--- scripts/02_locust.sh
+++ scripts/02_locust.sh
@@ -1,1 +1,6 @@
-locust -f src/locust.py --headless --html results/base.html -u 10 -r 1 -t 2m
+sleep 1
+for n_users in 10 20 50;do
+    rate=$((n_users / 10))
+    locust -f src/locust.py --headless --html results/$n_users.html -u $n_users -r $rate -t 2m
+    sleep 5
+done
